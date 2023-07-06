03e546face72061e6ce339c871b775b3140a7910
Gilles Bruno <gilles_bruno@gmx.fr>
2023-07-06 09:38:46 +0200

    Update AWS EC2 instance install file

--- install_ec2_instance.sh
+++ install_ec2_instance.sh
@@ -3,34 +3,34 @@
 # Step1 : Use user 'root'
 sudo su
 
-# Step1 : Install CURL
+# Step2 : Install CURL
 apt install curl
 
 
-# Step2 : Install NVM
+# Step3 : Install NVM
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 source ~/.bashrc
 
-# Step3 : Install Node
+# Step4 : Install Node
 nvm install node
 
-# Step4 : Install Specifiv Node Version
+# Step5 : Install Specifiv Node Version
 nvm install 18.16.0 
 
-# Step5 : Install Git
+# Step6 : Install Git
 apt update
 apt install git-all
 
-# Step6 : Install PM2
+# Step7 : Install PM2
 npm install pm2 -g
 
-# Step7 : Clone GIT repository
+# Step8 : Clone GIT repository
 git clone https://github.com/gilbruno/Woken_Discord_Bot.git
 
-# Step8 : Install NPM packages
+# Step9 : Install NPM packages
 npm install
 
-# Step9 : Compile the app
+# Step10 : Compile the app
 npm run build
 
 
